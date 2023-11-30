966a50dec883c0cb308ae7e2307ba42826b37d27
IlyesOul <81724457+IlyesOul@users.noreply.github.com>
2023-11-30 15:21:38 -0500

    Screwing around and found out

--- Create_Code/experimental.py
+++ Create_Code/experimental.py
@@ -1,27 +1,34 @@
-#
-# Licensed under 3-Clause BSD license available in the License file. Copyright (c) 2021-2022 iRobot Corporation. All rights reserved.
-#
-
-# This example explores the robot's LED lights.
-
 from irobot_edu_sdk.backend.bluetooth import Bluetooth
 from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
-from irobot_edu_sdk.music import Note
 
-robot = Root(Bluetooth())
-#robot = Create3(Bluetooth())
+robot = Create3(Bluetooth())
 
-time = 0.3
+global obstacle_position_array
+obstacle_position_array = []
 
+async def try_again (x, y):
+    global obstacle_position_array
+    obstacle_position_array.append([x,y])
+    await robot.turn_left(45)
 
 @event(robot.when_play)
 async def play(robot):
     while True:
-        await robot.set_lights_rgb(255, -2344.43, 0)  # Red: negative numbers will count as 0.
-        await robot.wait(time)
-        await robot.set_lights_rgb(0, 120.4, 0)  # Green: floating point numbers will be rounded.
-        await robot.wait(time)
-        await robot.set_lights_rgb(-1000, 0, 100)  # Blue
-        await robot.wait(time)
+        distance_array = (await robot.get_ir_proximity()).sensors
+        while distance_array[3] < 25:
+            print(f"Distance [3] = {distance_array[3]}")
+            await robot.set_wheel_speeds(10,10)
+            distance_array = (await robot.get_ir_proximity()).sensors
+        await try_again((await robot.get_position()).x, (await robot.get_position()).y)
+        global obstacle_position_array
+        while [(await robot.get_position()).x, (await robot.get_position()).y] not in obstacle_position_array and distance_array[3] < 25:
+            await robot.set_wheel_speeds(10,10)
+        await robot.move(-15)
+
+@event(robot.when_touched, [])
+async def when_touched(robot):
+    robot.set_wheel_speeds(0,0)
+    robot.set_lights_rgb(65,65,65)
+
 
 robot.play()
