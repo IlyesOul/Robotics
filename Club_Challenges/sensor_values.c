a13b7485b4022b94ce27a977c7e75b54e52777c5
IlyesOul <81724457+IlyesOul@users.noreply.github.com>
2023-12-13 09:31:32 -0500

    Update sensor_values.c

--- Club_Challenges/sensor_values.c
+++ Club_Challenges/sensor_values.c
@@ -3,9 +3,9 @@
 int main()
 {
     // Proximity sensor 
-    bool val = analog(0) < 1750; /*Object relatively close*/
-    val = analog(0) < 2000; /*Object close*/
-    val = analog(0) < 1750; /*Object really close*/
+    bool val = analog(0) < 1650; /*Object relatively close*/
+    val = analog(0) < 1750; /*Object close*/
+    val = analog(0) < 2200; /*Object really close*/
 
     //Light sensor
     val = digital(0) == 0 /*Returns 1 if light shined*/
