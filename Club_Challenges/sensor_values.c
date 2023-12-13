51b0570f857aed374c1b5d6720e059f556e429b7
IlyesOul <81724457+IlyesOul@users.noreply.github.com>
2023-12-12 20:15:56 -0500

    Started Sensor Documentation

---
+++ Club_Challenges/sensor_values.c
@@ -0,0 +1,18 @@
+#include <kipr/wombat.h>
+
+int main()
+{
+    // Proximity sensor 
+    bool val = analog(0) < 1750; /*Object relatively close*/
+    val = analog(0) < 2000; /*Object close*/
+    val = analog(0) < 1750; /*Object really close*/
+
+    //Light sensor
+    val = digital(0) == 0 /*Returns 1 if light shined*/
+
+    //Button
+    val = digital(0) == 0 /*Returns 1 if button is pressed*/
+
+    //Tophat (Yet to figure out)
+    
+}
