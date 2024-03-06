a2342cef914fc3b95589ecaa492935617cdefb53
IlyesOul <81724457+IlyesOul@users.noreply.github.com>
2024-03-06 15:28:56 -0500

    Update roomba_movement.c

--- Botball_2024/Create_Roomba/roomba_movement.c
+++ Botball_2024/Create_Roomba/roomba_movement.c
@@ -4,20 +4,35 @@ int main()
 {
     create3_connect();
     
-    int black_counter = 0;
+    /*int black_counter = 0;
     
     while(black_counter != 2)
     {
         create3_velocity_set_components(-.25, 0);
-        if(analog(0) > 3000)
+        
+        int analog_val = analog(0);
+        //If black is seen
+        if(analog_val > 3300)
         {
             black_counter+=1;
+            printf("nig1 detected");
+            analog_val = analog(0);
         }
-        while(analog(0) > 3000){create3_velocity_set_components(-.25, 0);}
+        //While black is seen move
+        while(analog_val > 3300){
+  			
+         	if ((black_counter == 2))
+                break;
+            create3_velocity_set_components(-.25, 0);
+            analog_val = analog(0);
+        }
         
-    }
+    }*/
     
-    create3_drive_straight(1, -0.25);
+    //Rotate
+    //wait_for_milliseconds(3000);
+    while()
+    	create3_rotate_degrees(-90, 180);
     
     return 0;
 }
