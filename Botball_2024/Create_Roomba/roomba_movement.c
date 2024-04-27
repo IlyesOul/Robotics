7e9f49955db5a5e4b0f0857ca0272537d1f186ae
tg-24 <150836078+tg-24@users.noreply.github.com>
2024-04-27 09:08:20 -0400

    Update roomba_movement.c

--- Botball_2024/Create_Roomba/roomba_movement.c
+++ Botball_2024/Create_Roomba/roomba_movement.c
@@ -57,44 +57,50 @@ int main()
     {
         create3_velocity_set_components(-.05, 0);
     }
-
-    create3_drive_straight(.07,0.75);
+    
+    //Over the hump
+    //create3_drive_straight(.07,0.75);
     
     //SUBSECTION: ROTATE TO TOWER
-    create3_rotate_degrees(-100, 180);
+    //create3_rotate_degrees(-100, 180);
     //Phillip idea
-    //create3_rotate_degrees(-90, 270);
+    create3_rotate_degrees(-90, 270);
     create3_wait();
 
     //SUBSECTION: Move towards tower
-    create3_drive_straight(.275,0.5);
+    create3_drive_straight(.26,0.5);
     create3_wait();
 
     //TODO - Pickup cube
     msleep(2000);
-    lower2(1050, 500);
+    lower2(1600, 500);
     msleep(1000);
     //Phillip idea
-    //set_servo_position(0, 1600);
     set_servo_position(0, 1500);
+    //set_servo_position(0, 1500);
     msleep(500);
 
     //Back up to pole
     create3_drive_straight(-.2, .25);
+    create3_wait();
 
     //Rotate towards noodle corner
-    create3_rotate_degrees(-80, 180);
+    create3_rotate_degrees(-90, 180);
+    create3_wait();
     
     //Delay for other bots
     create3_drive_straight(-.3, .25);
+    create3_wait();
     
     msleep(25000);
     
     //move towards noodle
     create3_drive_straight(.8, .25);
+    create3_wait();
     
     //Rotate towards rock heap
     create3_rotate_degrees(60, 180);
+    create3_wait();
 
     //Move towards rock heap
     create3_drive_straight(.35, .50);
@@ -104,26 +110,32 @@ int main()
     set_servo_position(0, 250);
 
     //Rotate to octagon
-    create3_rotate_degrees(90, 180);
+    create3_rotate_degrees(91, 180);
+    create3_wait();
 
     //Move forward
     create3_drive_straight(.15,.25);
+    create3_wait();
 
     //Rotate left
     create3_rotate_degrees(35, 180);
-
+    create3_wait();
+	
     //Move forward a bit more
     create3_drive_straight(.26, .25);
-
+    create3_wait();
+	
     //Increase height of motor2
     lift2(800,500);
 
     //Rotate right to smack cube
     create3_rotate_degrees(-80, 50);
-
+    create3_wait();
+	
     //Move Forward
     create3_drive_straight(.07, .25);
-
+    create3_wait();
+	
     //Move Backwards
     //create3_drive_straight(-.07, .25);
 
@@ -138,15 +150,17 @@ int main()
     
     //move backwards
     create3_drive_straight(-.1, .25);
-    
+    create3_wait();
+	
     //rotate next to middle cubes
     create3_rotate_degrees(18, 75);
+    create3_wait();
     
     //lower 2nd arm to level with cubes
     lower2(2000,500);
     
     //push cubes off to zone
-	create3_drive_straight(.1, .25);
+    create3_drive_straight(.1, .25);
     create3_wait();
     
     //lift 2nd arm
@@ -158,12 +172,14 @@ int main()
     
     //Rotate left to smack left cube
     create3_rotate_degrees(-115, 270);
+    create3_wait();
     
     //Lower arm 
     lower2(1800, 500);
     
     //Rotate left to hit other cube
     create3_rotate_degrees(270, 180);
+    create3_wait();
     
     
     return 0;
